edc077e3f1876bd57193e734009775a69b7fee1e
Ramanjs <raman.s2662@gmail.com>
2024-10-16 12:20:01 +0530

    make IFID reset async

--- IFID.v
+++ IFID.v
@@ -8,7 +8,7 @@ module IFID (
     output reg [31:0] instr_address_out
 );
 
-  always @(posedge clk) begin
+  always @(posedge clk or posedge reset) begin
     if (reset == 1'b1 || flush == 1'b1) begin
       instruction_out   <= 32'd0;
       instr_address_out <= 32'd0;
