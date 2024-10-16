0ab00856c2e07ee81cc5b928778d78f7d345efb1
Ramanjs <raman.s2662@gmail.com>
2024-10-16 23:52:11 +0530

    create register files for spike and weight vectors

---
+++ src/WVR.v
@@ -0,0 +1,15 @@
+module WVR (
+    input         clk,
+    input         we,
+    input  [ 4:0] ra,
+    input  [ 4:0] wa,
+    input  [31:0] wd,
+    output [31:0] rd
+);
+
+  reg [31:0] rf[31:0];
+  always @(posedge clk) if (we) rf[wa] <= wd;
+
+  assign rd = rf[ra];
+
+endmodule
