aa29e7971c0bb32664b76471897c0841b929a399
Ramanjs <raman.s2662@gmail.com>
2024-10-23 15:17:04 +0530

    add read port for 512 bit data

--- src/data_memory.v
+++ src/data_memory.v
@@ -1,13 +1,24 @@
 module data_memory (
-    input             clk,
-    input      [31:0] wd,
-    input      [31:0] a,
-    input             we,
-    output reg [31:0] rd
+    input              clk,
+    input      [ 31:0] wd,
+    input      [ 31:0] a,
+    input              we,
+    output reg [ 31:0] rd1,
+    output reg [511:0] rd2
 );
-
-  reg [31:0] RAM[127:0];
-  assign rd = RAM[a[31:2]];
+  genvar i;
+  reg [31:0] RAM[128];
+  assign rd1 = RAM[a[31:2]];
+  for (i = 0; i < 16; i = i + 1) begin
+    assign rd2[32*i+:32] = RAM[a[31:2]+i];
+  end
 
   always @(posedge clk) if (we) RAM[a[31:2]] <= wd;
+  integer j;
+  initial begin
+    for (j = 0; j < 16; j = j + 1) begin
+      RAM[j] = j;
+    end
+    $writememh("memh.txt", RAM);
+  end
 endmodule
