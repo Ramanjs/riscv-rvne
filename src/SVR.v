b80e3d1e1fe62e44abbf06d77f32442194de8e71
Ramanjs <raman.s2662@gmail.com>
2024-10-23 15:16:18 +0530

    add support for 512b writes

--- src/SVR.v
+++ src/SVR.v
@@ -1,15 +1,29 @@
 module SVR (
-    input         clk,
-    input         we,
-    input  [ 4:0] ra,
-    input  [ 4:0] wa,
-    input  [31:0] wd,
-    output [31:0] rd
+    input          clk,
+    input          we,
+    input  [  1:0] VL,
+    input  [  4:0] ra,
+    input  [  4:0] wa,
+    input  [511:0] wd,
+    output [ 31:0] rd
 );
 
-  reg [31:0] rf[31:0];
-  always @(posedge clk) if (we) rf[wa] <= wd;
+  reg     [31:0] rf[31];
+  integer        i;
+  always @(posedge clk) begin
+    if (we) begin
+      if (VL == 2'b00) rf[wa] <= wd[31:0];
+      else if (VL == 2'b01) begin
+        for (i = 0; i < 4; i = i + 1) begin
+          rf[wa+i] <= wd[32*i+:32];
+        end
+      end else if (VL == 2'b10) begin
+        for (i = 0; i < 16; i = i + 1) begin
+          rf[wa+i] <= wd[32*i+:32];
+        end
+      end
+    end
+  end
 
   assign rd = rf[ra];
-
 endmodule
